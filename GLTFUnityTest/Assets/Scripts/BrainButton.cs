567e6d14f5cb518aed33cc58a5e19912cd181ccc
Brynimation <zcabbll@ucl.ac.uk>
2020-11-18 14:44:39 +0000

    Setting up unity project. Have a test I did for loading a GLTF model at runtime

---
+++ GLTFUnityTest/Assets/Scripts/BrainButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BrainButton : MonoBehaviour
+{
+    public void loadBrain(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
---
+++ GLTFUnityTest/Assets/Scripts/ModelLoader.cs
@@ -0,0 +1,24 @@
+﻿using Siccity;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+//loads the model at runtime
+
+public class ModelLoader : MonoBehaviour
+{
+    string curDir = Directory.GetCurrentDirectory();
+    string man = "Assets\\SampleGLTF\\Man\\source\\dAb20200729.gltf";
+    GameObject male;
+
+    void Start()
+    {
+        male = Siccity.GLTFUtility.Importer.LoadFromFile(man);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
