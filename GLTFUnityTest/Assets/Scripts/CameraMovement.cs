da97c19742a640899d9a06b2b3cbb28c95b2c8b7
Brynimation <zcabbll@ucl.ac.uk>
2020-12-01 11:55:54 +0000

    camera controls v1 in scene cameraTest

---
+++ GLTFUnityTest/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Camera cam;
+    private Vector3 prevPosition;
+    public Transform target;
+    private Vector3 displacement; 
+    private Vector3 dirVec;
+    private float cameraDistance = -250f;
+    private float scrollSpeed = 250f;
+    void Start() 
+    {
+        displacement = new Vector3(0, 0, cameraDistance);
+        dirVec = new Vector3();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //target.transform.position = cam.ScreenToViewportPoint(Input.mousePosition);
+        
+        if(Input.GetMouseButtonDown(0)){
+            prevPosition = cam.ScreenToViewportPoint(Input.mousePosition); // cam position set to normalised version of mouse coord
+        }
+        if(Input.GetMouseButton(0)){
+            Vector3 dir = prevPosition - cam.ScreenToViewportPoint(Input.mousePosition);
+
+            cam.transform.position = target.transform.position;
+
+            cam.transform.Rotate(new Vector3(1f, 0f, 0f), dir.y *180);
+            cam.transform.Rotate(new Vector3(0f, 1f, 0f), -dir.x * 180, Space.World);
+            cam.transform.Translate(displacement);
+            prevPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+
+        }else if(Input.GetAxis("Mouse ScrollWheel") != 0){
+            cam.transform.position = target.transform.position;
+            float scrollAmount = Input.GetAxis("Mouse ScrollWheel")*scrollSpeed;
+            displacement -= new Vector3(0, 0, scrollAmount);
+            print(displacement);
+            cam.transform.Translate(displacement);
+        }
+
+        
+    }
+}
+ 
