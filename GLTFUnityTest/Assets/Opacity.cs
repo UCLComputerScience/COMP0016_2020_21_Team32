f1e21e41bcfed83ccd10b93f0c07aa8855348454
c499 <55796240+c499@users.noreply.github.com>
2020-12-03 18:08:02 +0000

    Functioning opacity slider for outermost layer
    Proof of concept for how opacity can be done.

---
+++ GLTFUnityTest/Assets/Opacity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opacity : MonoBehaviour
+{
+    // public float SegOpacity = 0.5f;
+
+    // public void AdjustOpacity(float newOp) {
+    //     SegOpacity = newOp;
+    // }
+
+    //moved the function of this script to LoadBrain.cs instead, feel free to delete Opacity.cs
+
+}
