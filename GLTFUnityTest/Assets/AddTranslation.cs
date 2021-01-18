f4203a814a529e428c0698e83367d26ff77bdd0c
Brynimation <zcabbll@ucl.ac.uk>
2021-01-18 17:53:58 +0000

    Colour wheel now spins when you click and drag it (very

---
+++ GLTFUnityTest/Assets/AddTranslation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AddTranslation : MonoBehaviour
+{
+    public LineRenderer xAxis;
+    public LineRenderer yAxis;
+
+    public LineRenderer zAxis;
+
+    [SerializeField] GameObject translate;
+    
+    bool isEnabled = false;
+
+    void Start()
+    {
+        subscribeToEvents();
+        xAxis = new LineRenderer();
+        yAxis = new LineRenderer();
+        zAxis = new LineRenderer();
+        translate.SetActive(false);
+        
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isEnabled)return;
+
+        
+    }
+    public void subscribeToEvents(){
+        SelectionManager.current.onCameraButtonPressed += otherEvent;
+        SelectionManager.current.onTButtonPressed += SelectionManager_OnTButtonPressed;
+        SelectionManager.current.onRButtonPressed += otherEvent;
+        SelectionManager.current.onReButtonPressed += otherEvent;
+        SelectionManager.current.onVAnnotationButtonPressed += otherEvent;
+        SelectionManager.current.onAnnotationButton += otherEvent;
+    }
+
+    private void SelectionManager_OnTButtonPressed(object sender, EventArgs e){
+        print("Here!");
+        translate.SetActive(true);
+        translate.transform.localScale = new Vector3(10, 10, 10);
+    }
+    private void otherEvent(object sender, EventArgs e){
+        translate.SetActive(false);
+        isEnabled = false;
+    }
+
+}
