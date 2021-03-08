d9c8d17a1a7184e4200f0b835bd3255604393b2e
Brynimation <zcabbll@ucl.ac.uk>
2021-03-08 12:08:46 +0000

    First draft of readme and added UnityTestRunner package for unit testing

---
+++ HoloRepositoryPortable2021/Assets/Scripts/Model loading and interaction/BrainExample.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ ///<summary>This class is instantiated if the example model of a brain is loaded in</summary>
+public class BrainExample : Organ
+{   
+    public BrainExample(GameObject model){
+        base.model = model;
+        base.centrePos = new Vector3(-94.2f, -99.23f, -93.6f);
+        base.centreRot = Quaternion.Euler(0.453f, -288.9f, 1.323f);
+        base.segments = new List<GameObject>();
+    }
+    public override void initialiseModel(GameObject parent)
+    {
+        GameObject temp = null;
+        int count = 0;
+        model.transform.SetParent(parent.transform);
+        model.transform.localPosition = centrePos;
+        model.transform.localRotation = centreRot;
+        foreach(Transform child in model.transform){
+            if(child.gameObject.GetComponent<Renderer>() != null && count != 1)segments.Add(child.gameObject);
+            if(count ==1) temp = child.gameObject;
+            else if(child.gameObject.GetComponent<Camera>() != null) child.gameObject.SetActive(false);
+            count++;   
+        }
+        segments.Add(temp);
+    }
+
+}
---
+++ HoloRepositoryPortable2021/Assets/Tests/EventTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+using System.IO;
+
+namespace Tests
+{
+    public class EventTests
+    {
+
+
+
+        [UnityTest]
+        public IEnumerator EventManagerTest_onCameraEnabled()
+        {
+            GameObject publisher = new GameObject();
+            GameObject model = new GameObject();
+            GameObject eventListener = new GameObject();
+            GameObject slider = new GameObject("Opacity Slider");
+            GameObject plane = new GameObject("Plane");
+            GameObject pivot = new GameObject("Pivot");
+            GameObject camera = new GameObject("MainCamera");
+            var cam = camera.AddComponent<Camera>();
+            cam.tag = "MainCamera";
+        
+
+            slider.AddComponent<Slider>();
+            
+            var modelHandler = model.AddComponent<ModelHandler>();
+            Organ organ = OrganFactory.GetOrgan(Application.streamingAssetsPath + Path.DirectorySeparatorChar+ "brain.glb");
+            organ.initialiseModel(model);
+            ModelHandler.organ = organ;
+            
+            var cameraController = eventListener.AddComponent<CameraController>();
+            cameraController.pivot = pivot;
+            cameraController.isEnabled = false;
+
+            var eventManager = publisher.AddComponent<EventManager>();
+            eventManager.onEnableCamera();
+            
+
+            yield return new WaitForSeconds(1f);
+
+            Assert.True(cameraController.isEnabled);
+            
+        }
+    }
+}
